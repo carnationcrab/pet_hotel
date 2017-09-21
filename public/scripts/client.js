b001d804269e1d6fae7711343c1ab60f692a28c8
Evan Kearney <evanjkearney@gmail.com>
2017-09-21 14:52:44 -0500

    did the angularjs

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,4 +4,25 @@ var ourApp = angular.module('ourApp', []);
 
 ourApp.controller('HotelController', function($http){
     console.log('NG');
+
+    var vm = this;
+    
+    vm.addPet = function(){
+        console.log('IN ADD PET:', vm.name, vm.breed, vm.color);
+        // $http ({
+        //     method: 
+        // })
+    }
+    
+    vm.getPet = function() {
+        console.log('in get');
+        $http({
+            method: 'GET',
+            url: '/pet',
+        }) .then( function(response){
+            console.log('back from server with:', response);
+            vm.pets = response.data;
+        })
+    }
+    
 })
